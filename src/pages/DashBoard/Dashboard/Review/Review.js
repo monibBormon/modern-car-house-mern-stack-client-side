b6e2ed49e7a112a91f5fd3c54ce859209f474432
monibBormon <monibbormon14@gmail.com>
2021-11-13 23:23:09 +0600

    added rating

--- src/pages/DashBoard/Dashboard/Review/Review.js
+++ src/pages/DashBoard/Dashboard/Review/Review.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa'
 
 const Review = () => {
+    const [rating, setRating] = useState(null)
+    const [hover, setHover] = useState(null)
     return (
         <div>
-            <h2 className='text-4xl'>Review Our Service</h2>
+            <h2 className='text-4xl mb-5'>Review Our Service</h2>
+            <div>
+                {[...Array(5)].map((star, i) => {
+                    const ratingValue = i + 1;
+                    return <label key={i}>
+                        <input type="radio" name="rating" value={ratingValue} onClick={() => setRating(ratingValue)} />
+                        <FaStar color={ratingValue <= (hover || rating) ? '#ffc107' : 'e4e5e9'} className='star inline-block text-4xl' onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)}></FaStar>
+                    </label>
+                })}
+            </div>
+            <textarea className='border-2 w-1/2 h-16 border-red-400 pl-2 pt-2 mt-3 rounded-lg' name="rating" cols="30" rows="5" placeholder='Give your feedback'></textarea><br />
+            <button className='border-2 border-red-400 rounded-full px-5 py-1 font-semibold'>Submit Rating</button>
         </div>
     );
 };
